2182268e01d6c776478c1bdee22f432fd2836e57
MikkoKarvonen <mkarvonen34@gmail.com>
2019-04-03 17:46:10 +0300

    Lisätty tehtävä 2.4

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -3,30 +3,49 @@ import ReactDOM from 'react-dom'
 import Course from './components/Course'
 
 const App = () => {
-    const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [
-            {
-                name: 'Reactin perusteet',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Tiedonvälitys propseilla',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'Komponenttien tila',
-                exercises: 14,
-                id: 3
-            }
-        ]
-    }
+    const courses = [
+        {
+            name: 'Half Stack -sovelluskehitys',
+            id: 1,
+            parts: [
+                {
+                    name: 'Reactin perusteet',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Tiedonvälitys propseilla',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'Komponenttien tila',
+                    exercises: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewaret',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
     return (
         <div>
-            <Course course={course} />
+            {courses.map(course => <Course course={course} />)}
         </div>
     )
 }
