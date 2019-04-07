b3419b42ef673558401f98167c25b033b9277b6b
MikkoKarvonen <mkarvonen34@gmail.com>
2019-04-07 09:10:49 +0300

    Lisätty tehtävä 2.13

--- maat/src/App.js
+++ maat/src/App.js
@@ -3,8 +3,9 @@ import axios from 'axios'
 import './App.css'
 
 const App = () => {
-  const [countries, setCountries] = useState([]) 
-  const [ toShow, setToShow ] = useState('')
+  const [countries, setCountries] = useState([])
+  const [toShow, setToShow] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   useEffect(() => {
     axios
@@ -15,35 +16,47 @@ const App = () => {
   }, [])
 
   const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
     const val = event.target.value;
     var result = countries.filter(obj => {
       return obj.name.toLowerCase().includes(val.toLowerCase())
     })
-    if (result.length > 10){
+    if (result.length > 10) {
       setToShow(<p>Too many countries, specify another filter</p>)
     } else {
-      if (result.length === 1){
+      if (result.length === 1) {
+        showCountry(result[0])
+      } else {
         setToShow(result.map((res) => (
           <div key={res.name}>
-            <h1>{res.name}</h1>
-            <p>capital {res.capital}</p>
-            <p>population {res.population}</p>
-            <h2>languages</h2>
-            <ul>
-              {res.languages.map((lang) => <li key={lang.name}>{lang.name}</li>)}
-            </ul>
-            <img src={res.flag} alt={res.name}/>
+            <p>
+              {res.name}
+              <button onClick={() => showCountry(res)}>show</button>
+            </p>
           </div>
         )))
-      } else {
-        setToShow(result.map((res) => <p key={res.name}>{res.name}</p>))
       }
     }
   }
 
+  const showCountry = (res) => {
+    setToShow(() => (
+      <div>
+        <h1>{res.name}</h1>
+        <p>capital {res.capital}</p>
+        <p>population {res.population}</p>
+        <h2>languages</h2>
+        <ul>
+          {res.languages.map((lang) => <li key={lang.name}>{lang.name}</li>)}
+        </ul>
+        <img src={res.flag} alt={res.name} />
+      </div>
+    ))
+  }
+
   return (
     <div>
-      find countries <input  onChange={handleSearchChange} />
+      find countries <input value={newSearch} onChange={handleSearchChange} />
       {toShow}
     </div>
   );
